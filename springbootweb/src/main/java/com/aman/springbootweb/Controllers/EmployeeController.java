46d602972e07bfa16ee55c1e0a7a456e2697c6f3
Aman Rana <ar334060@gmail.com>
2026-06-03 00:06:37 +0530

    Controllers and @PathVariable

--- springbootweb/src/main/java/com/aman/springbootweb/Controllers/EmployeeController.java
+++ springbootweb/src/main/java/com/aman/springbootweb/Controllers/EmployeeController.java
@@ -1,18 +1,39 @@
 package com.aman.springbootweb.Controllers;
 
 import com.aman.springbootweb.DTO.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
-
+@RequestMapping("/employee")
 public class EmployeeController {
-    @GetMapping("/employee/{employeeId}")
+    @GetMapping("/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
         return new EmployeeDTO(employeeId,"Aman","ar334060",22, LocalDate.of(2026,6,23),true);
+    }
+
+
+    @GetMapping //  we can remove as added the employee as parent path  using @RequestMapping/employee
+    public String getEmployeeAge(@RequestParam(required = false) Integer age,@RequestParam (required = false) String sortBy){
+        return "Hi my age is : "+age + " "+sortBy;
+    }
+
+    @PostMapping
+    public String postReq(){
+        return "hello";
+    }
+
+    @PostMapping("/setEmp")
+    public EmployeeDTO createEmployee(@RequestBody EmployeeDTO inputEmp){
+        inputEmp.setId(100L);
+        return inputEmp;
+    }
+
+
+    @PutMapping
+    public String putReq(){
+        return "putted";
     }
 
 }
